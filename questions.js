dc193fddefd98d2e3b78560a6c2f79ae677759d9
Karolína Šebestová <ka.sebestova.cz@gmail.com>
2025-05-25 17:33:32 +0200

    reorganize

---
+++ questions.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        question: "Jaká je hlavní město Francie?",
+        options: ["Berlín", "Madrid", "Paříž", "Řím"],
+        answer: "Paříž"
+    },
+    {
+        question: "Jaký je nejvyšší vrchol světa?",
+        options: ["K2", "Mount Everest", "Kangchenjunga", "Lhotse"],
+        answer: "Mount Everest"
+    },
+    {
+        question: "Který z níže uvedených obrázků má houslový klíč?",
+        images: ["obrazek1.jpg", "obrazek2.jpg", "obrazek3.jpg", "obrazek4.jpg"],
+        answer: "obrazek2.jpg"
+    },
+    {
+        question: "Jaký zvuk slyšíte?",
+        audio: "zvuk.mp3",
+        options: ["Ptačí zpěv", "Šumění moře", "Zvuk deště", "Zvuk větru"],
+        answer: "Ptačí zpěv"
+    },
+    {
+        question: "Jaký je tento hudební nástroj?",
+        questionImage: "media/nastroj.png",
+        options: ["Kytara", "Klavír", "Housle", "Flétna"],
+        answer: "Housle"
+    },
+    {
+        question: "Které z těchto zvířat je savec?",
+        options: ["Pštros", "Želva", "Krokodýl", "Velryba"],
+        answer: "Velryba"
+    }
+];
